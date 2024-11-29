9dd1dea12ed884730a22d26ec8c36d26999e0cfd
Waleed Amgad <waleedbookey@gmail.com>
2024-11-30 00:17:10 +0200

    removed modules

--- p5_shaders/first_project/sketch.js
+++ p5_shaders/first_project/sketch.js
@@ -1,9 +1,17 @@
-import p5 from "p5"
+
 
 function setup() {
-
-}
-
-function draw(){
+    createCanvas(500, 500, WEBGL);
     
-}
+    }
+  
+  function draw() {
+    background (200);
+  
+    fill(255,255,0);
+    
+    let bitesize=PI/8;
+    let startangle=bitesize*sin(frameCount*0.05)+bitesize;
+    let endangle=2*PI;
+    arc(25,25,50,70,startangle,endangle,PIE);
+  }
